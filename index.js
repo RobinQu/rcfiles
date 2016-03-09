87a4d2d623387308f9b87c5a715fd533f3e8aaf7
long.qul <long.qul@alibaba-inc.com>
2016-03-09 20:21:36 +0800

    new files

---
+++ index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  'es6': path.resolve(__dirname, 'resources', 'lintrc_es6'),
+  'es6_babel': path.resolve(__dirname, 'resources', 'lintrc_es6_babel')
+};
